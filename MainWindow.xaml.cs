35e8fc20e05501bdbde7c33da5b472f757bf1627
weilai5201314 <w200111040833@163.com>
2023-04-19 23:22:32 +0800

    构建按钮，初始化界面

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lunch_Select
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // 设置了全屏的组件适配
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 获取窗口的宽度和高度
+            double width = MyCanvas.ActualWidth;
+            double height = MyCanvas.ActualHeight;
+
+            // 设置按钮的Canvas.Left和Canvas.Top属性，使其居中显示
+            Button btn = MyCanvas.Children[0] as Button;
+            Canvas.SetLeft(btn, (width - btn.ActualWidth) / 2);
+            Canvas.SetTop(btn, (height - btn.ActualHeight) / 2);
+        }
+    }
+}
