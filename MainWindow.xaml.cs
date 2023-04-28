3b4844865eb27f1b328d603ac4bd7507acc08d43
weilai5201314 <w200111040833@163.com>
2023-04-28 11:54:04 +0800

    成功完成用户界面对数据库的写入写出功能

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -51,7 +51,7 @@ namespace Lunch_Select
         private void ButtonMain_Click(object sender, RoutedEventArgs e)
         {
             //MessageBox.Show("Hello world.恭喜进入选菜按钮事件");
-            //开始连接数据库
+            //  开始连接数据库
             string connStr =
                 "server=localhost;port=3306;user=root;database=test;port=3306;password=12345678;";
             MessageBox.Show("Connecting to MySQL..."); // 添加这行代码
@@ -60,21 +60,31 @@ namespace Lunch_Select
             {
                 conn.Open();
                 MessageBox.Show("Success connecting Mysql !");
-                
-                // 开始数据库的操作
-                string query = "SELECT 菜名,口味 FROM test.菜单表";
-                string result = "";
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                MySqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    result += reader.GetString(0) + "\t" + reader.GetString(1) + Environment.NewLine;
-                }
 
-                reader.Close();
-                MyTextBox1.Text = result;
+                //  开始数据库的操作
+                //  读取信息
+                // string query = "SELECT 菜名,口味 FROM test.菜单表";
+                // string result1 = "";
+                // string result2 = "";
+                // MySqlCommand cmd = new MySqlCommand(query, conn);
+                // MySqlDataReader reader = cmd.ExecuteReader();
+                // while (reader.Read())
+                // {
+                //     result1 += reader.GetString(0) + Environment.NewLine;
+                //     result2 += reader.GetString(1) + Environment.NewLine;
+                // }
+                // reader.Close();
+                // MyTextBox1.Text = result1;
+                // MyTextBox2.Text = result2;
+
+                //  写入操作
+                string menuName = MyTextBox1.Text;
+                string flavor = MyTextBox2.Text;
+                string insertQuery = "INSERT INTO test.菜单表 (菜名, 口味) VALUES ('" + menuName + "', '" + flavor + "')";
+                MySqlCommand cmd = new MySqlCommand(insertQuery, conn);
+                cmd.ExecuteNonQuery();
             }
-            catch (Exception ex )
+            catch (Exception ex)
             {
                 // Console.WriteLine(exception);
                 throw;
