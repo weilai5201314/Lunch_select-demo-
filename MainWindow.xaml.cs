d0cfa93a55a02df57e8b7450f46ab45973c1e269
weilai5201314 <w200111040833@163.com>
2023-04-29 12:43:01 +0800

    修改UI,修改了读取数据库参数

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,17 @@
-﻿// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Text;
-// using System.Threading.Tasks;
-
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-// using System.Windows.Data;
-// using System.Windows.Documents;
-// using System.Windows.Input;
-// using System.Windows.Media;
-// using System.Windows.Media.Imaging;
-// using System.Windows.Navigation;
-// using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 //引用数据库
 using MySql.Data.MySqlClient;
 
@@ -22,24 +20,38 @@ namespace Lunch_Select
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
+        
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         // 设置了全屏的组件适配
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             // 获取窗口的宽度和高度
-            Button btn = MyCanvas.Children[0] as Button; //似乎有小小的问题，但是无法解决
+
+            // 获取窗口的宽度和高度
+            double width = MyCanvas.ActualWidth;
+            double height = MyCanvas.ActualHeight;
+
+            // 设置按钮的宽度和高度为窗口的1/3
+            Button btn = MyCanvas.Children[0] as Button;
             if (btn != null)
             {
-                double width = MyCanvas.ActualWidth;
-                double height = MyCanvas.ActualHeight;
-                Canvas.SetLeft(btn, (width - btn.ActualWidth) / 2);
-                Canvas.SetTop(btn, (height - btn.ActualHeight) / 2);
+                btn.Width = width / 3;
+                btn.Height = height / 3;
+
+                // 设置按钮的位置为窗口中心
+                Canvas.SetLeft(btn, (width - btn.Width) / 2);
+                Canvas.SetTop(btn, (height - btn.Height) / 2);
             }
             else
             {
@@ -47,42 +59,49 @@ namespace Lunch_Select
             }
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         //  设置点击选菜按钮之后的跳转函数
+        //  包括了数据库的操作
         private void ButtonMain_Click(object sender, RoutedEventArgs e)
         {
             //MessageBox.Show("Hello world.恭喜进入选菜按钮事件");
             //  开始连接数据库
             string connStr =
                 "server=localhost;port=3306;user=root;database=test;port=3306;password=12345678;";
-            MessageBox.Show("Connecting to MySQL..."); // 添加这行代码
+
+            MessageBox.Show("Connecting Mysql......","提示");
+
+
             MySqlConnection conn = new MySqlConnection(connStr);
             try
             {
                 conn.Open();
-                MessageBox.Show("Success connecting Mysql !");
+                MessageBox.Show("Success connecting Mysql.","提示");
 
                 //  开始数据库的操作
                 //  读取信息
                 // string query = "SELECT 菜名,口味 FROM test.菜单表";
-                // string result1 = "";
-                // string result2 = "";
                 // MySqlCommand cmd = new MySqlCommand(query, conn);
                 // MySqlDataReader reader = cmd.ExecuteReader();
                 // while (reader.Read())
                 // {
-                //     result1 += reader.GetString(0) + Environment.NewLine;
-                //     result2 += reader.GetString(1) + Environment.NewLine;
+                //     MyTextBox1.Text += reader.GetString(0) + Environment.NewLine;
+                //     MyTextBox2.Text += reader.GetString(1) + Environment.NewLine;
                 // }
+                //
                 // reader.Close();
-                // MyTextBox1.Text = result1;
-                // MyTextBox2.Text = result2;
 
                 //  写入操作
                 string menuName = MyTextBox1.Text;
                 string flavor = MyTextBox2.Text;
                 string insertQuery = "INSERT INTO test.菜单表 (菜名, 口味) VALUES ('" + menuName + "', '" + flavor + "')";
-                MySqlCommand cmd = new MySqlCommand(insertQuery, conn);
-                cmd.ExecuteNonQuery();
+                MySqlCommand writing = new MySqlCommand(insertQuery, conn);
+                writing.ExecuteNonQuery();
+                MessageBox.Show("成功导入菜品。", "写入操作");
+
             }
             catch (Exception ex)
             {
